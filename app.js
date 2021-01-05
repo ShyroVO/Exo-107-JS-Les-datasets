c0eb2c58b98867edaf675f31f32a70e2a33cb2b2
ShyroVO <bosquet.laurie@outlook.com>
2021-01-05 16:49:16 +0100

    0.3 /

--- app.js
+++ app.js
@@ -4,9 +4,30 @@ let p2 = document.getElementById('p1');
 let p3 = document.getElementById('p1');
 let p4 = document.getElementById('p1');
 
+let pUn = (Number.parseFloat(p1.dataset.nombre) + 10);
+let pDeux = p2.innerHTML.length;
+
+let pTrois =
+
 // Elements pour lesquels vous créerez des datasets depuis JavaScript.
 let p5 = document.getElementById('p1');
 let p6 = document.getElementById('p1');
 let p7 = document.getElementById('p1');
 let p8 = document.getElementById('p1');
 
+
+
+//#p1. En JS, ajoutez 10 à la valeur récupérée et
+// mettez à jour le dataset de l'élément en JS aussi.
+
+//#p2. En JS, mettez à jour la valeur du dataset avec la
+// longeur de la chaine récupérée ( mettez dans le dataset
+// la longueur totale de la chaîne de caractères )
+
+//#p3. En JS, transformez le variable contenant la valeur du
+// dataset en tableau, et insérez ce tableau dans le innerHTML de l'élément #p3
+
+//#p4. En JS, Testez si la valeur est true,
+// si c'est le cas, indiquez "La valeur est vraie"
+// dans le innerHtml de l'élément #p4,
+// si ce n'est pas le cas, indiquer l'inverse.
