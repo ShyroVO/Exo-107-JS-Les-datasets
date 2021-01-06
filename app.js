1711fb82bd1b4af2fc136f0ea43032de336a3d49
ShyroVO <Bosquet.laurie@outlook.com>
2021-01-06 16:20:52 +0100

    1.0 / - Bonus

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 // Elements pour lesquels vous créerez des datasets directement dans le code HTML.
 let p1 = document.getElementById('p1');
-let p2 = document.getElementById('p1');
-let p3 = document.getElementById('p1');
-let p4 = document.getElementById('p1');
+let p2 = document.getElementById('p2');
+let p3 = document.getElementById('p3');
+let p4 = document.getElementById('p4');
 
 let pUn = (Number.parseFloat(p1.dataset.nombre) + 10);
-let pDeux = p2.innerHTML.length;
+p1.dataset.nombre = pUn;
 
-let pTrois =
+let pDeux = p2.innerHTML.length;
+p2.dataset.text = pDeux;
+
+let pTrois = p2.dataset.liste;
+p3.innerHTML += pTrois;
+
+if (p4.dataset.booleen === 'true') {
+    p4.innerHTML += ' La valeur est vraie.'
+}
+else {
+    p4.innerHTML += ' La valeur est fausse.';
+}
+
 
 // Elements pour lesquels vous créerez des datasets depuis JavaScript.
-let p5 = document.getElementById('p1');
-let p6 = document.getElementById('p1');
-let p7 = document.getElementById('p1');
-let p8 = document.getElementById('p1');
+let p5 = document.getElementById('p5');
+let p6 = document.getElementById('p6');
+let p7 = document.getElementById('p7');
+let p8 = document.getElementById('p8');
 
+p5.dataset.nombre = pUn ;
+p6.dataset.text = pDeux;
+p7.innerHTML += pTrois;
+p8.dataset.booleen = 'true';
 
-
-//#p1. En JS, ajoutez 10 à la valeur récupérée et
-// mettez à jour le dataset de l'élément en JS aussi.
-
-//#p2. En JS, mettez à jour la valeur du dataset avec la
-// longeur de la chaine récupérée ( mettez dans le dataset
-// la longueur totale de la chaîne de caractères )
-
-//#p3. En JS, transformez le variable contenant la valeur du
-// dataset en tableau, et insérez ce tableau dans le innerHTML de l'élément #p3
-
-//#p4. En JS, Testez si la valeur est true,
-// si c'est le cas, indiquez "La valeur est vraie"
-// dans le innerHtml de l'élément #p4,
-// si ce n'est pas le cas, indiquer l'inverse.
+if (p8.dataset.booleen === 'true'){
+    p8.innerHTML += ' La valeur est vraie.'
+}
+else {
+    p8.innerHTML += ' La valeur est fausse.';
+}
